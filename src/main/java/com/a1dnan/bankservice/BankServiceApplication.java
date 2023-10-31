0b5630ada75a384c763511bb539900d7aa5136bd
a1dnan <aaliladnane@gmail.com>
2023-10-31 11:40:56 +0100

    Add customer model

--- src/main/java/com/a1dnan/bankservice/BankServiceApplication.java
+++ src/main/java/com/a1dnan/bankservice/BankServiceApplication.java
@@ -1,8 +1,10 @@
 package com.a1dnan.bankservice;
 
 import com.a1dnan.bankservice.entities.BankAccount;
+import com.a1dnan.bankservice.entities.Customer;
 import com.a1dnan.bankservice.enums.AccountType;
 import com.a1dnan.bankservice.repos.BankAccountRepository;
+import com.a1dnan.bankservice.repos.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BankServiceApplication {
@@ -20,18 +23,28 @@ public class BankServiceApplication {
 
 
 	@Bean
-	CommandLineRunner start(BankAccountRepository bankAccountRepository){
+	CommandLineRunner start(BankAccountRepository bankAccountRepository, CustomerRepository customerRepository){
 		return args -> {
-			for(int i = 0 ; i<10 ;i++){
-				BankAccount bankAccount = BankAccount.builder()
-						.id(UUID.randomUUID().toString())
-						.type(Math.random()>0.5?AccountType.CURRENT_ACCOUNT:AccountType.SAVING_ACCOUNT)
-						.balance(10000*Math.random()*9000)
-						.createdAt(new Date())
-						.currency("MAD")
+			Stream.of("Adnan", "Mohamed").forEach(c->{
+				Customer customer = Customer.builder()
+						.name(c)
 						.build();
-				bankAccountRepository.save(bankAccount);
-			}
+				customerRepository.save(customer);
+			});
+
+			customerRepository.findAll().forEach(customer -> {
+				for(int i = 0 ; i<2 ;i++){
+					BankAccount bankAccount = BankAccount.builder()
+							.id(UUID.randomUUID().toString())
+							.type(Math.random()>0.5?AccountType.CURRENT_ACCOUNT:AccountType.SAVING_ACCOUNT)
+							.balance(10000*Math.random())
+							.createdAt(new Date())
+							.currency("MAD")
+							.customer(customer)
+							.build();
+					bankAccountRepository.save(bankAccount);
+				}
+			});
 		};
 	}
 }
--- src/main/java/com/a1dnan/bankservice/web/AccountGraphQLController.java
+++ src/main/java/com/a1dnan/bankservice/web/AccountGraphQLController.java
@@ -1,9 +1,15 @@
 package com.a1dnan.bankservice.web;
 
+import com.a1dnan.bankservice.dtos.BankAccountRequestDTO;
+import com.a1dnan.bankservice.dtos.BankAccountResponseDTO;
 import com.a1dnan.bankservice.entities.BankAccount;
+import com.a1dnan.bankservice.entities.Customer;
 import com.a1dnan.bankservice.repos.BankAccountRepository;
+import com.a1dnan.bankservice.repos.CustomerRepository;
+import com.a1dnan.bankservice.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -14,6 +20,8 @@ import java.util.List;
 public class AccountGraphQLController {
 
     private final BankAccountRepository bankAccountRepository;
+    private final AccountService accountService;
+    private final CustomerRepository customerRepository;
 
     @QueryMapping
     public List<BankAccount> accountsList(){
@@ -25,4 +33,25 @@ public class AccountGraphQLController {
         return bankAccountRepository.findById(id)
                 .orElseThrow(()-> new RuntimeException(String.format("Account %s not found",id)));
     }
+
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAccount(bankAccount);
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO updateAccount(@Argument String id, @Argument BankAccountRequestDTO bankAccount){
+        return accountService.updateAccount(bankAccount,id);
+    }
+
+    @MutationMapping
+    public void deleteAccount(@Argument String id){
+        accountService.deleteAccount(id);
+    }
+
+    @QueryMapping
+    public List<Customer> customers(){
+        return customerRepository.findAll();
+    }
+
 }
